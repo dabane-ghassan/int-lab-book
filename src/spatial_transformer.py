646ad47982d8f97bd861e08609885853b8db7069
dabane-ghassan <dabane.ghassan@gmail.com>
2021-04-09 11:54:13 +0200

    integrating lenet with spatial transformer

--- src/spatial_transformer.py
+++ src/spatial_transformer.py
@@ -14,11 +14,10 @@ class SpatialTransformer(nn.Module):
 
     def __init__(self):
         super(SpatialTransformer, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)
+        self.conv1 = nn.Conv2d(1, 20, 5, 1)
+        self.conv2 = nn.Conv2d(20, 50, 5, 1)
+        self.fc1 = nn.Linear(4*4*50, 500)
+        self.fc2 = nn.Linear(500, 10)
 
         # The localization-network
         self.localization = nn.Sequential(
@@ -59,10 +58,11 @@ class SpatialTransformer(nn.Module):
         x = self.stn(x)
 
         # Perform the usual forward pass
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = x.view(-1, 4*4*50)
         x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
