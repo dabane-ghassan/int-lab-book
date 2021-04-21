9d78d16a33ef4e0338ac40d35d4e519d4bda7b2c
dabane-ghassan <dabane.ghassan@gmail.com>
2021-04-21 10:57:35 +0200

    starting to create a new architecture

--- src/stn.py
+++ src/stn.py
@@ -18,8 +18,8 @@ class STN(nn.Module):
         super(STN, self).__init__()
         
         ## The what pathway
-        self.conv1 = nn.Conv2d(1, 20, 5, 1)
-        self.conv2 = nn.Conv2d(20, 50, 5, 1)
+        self.conv1 = nn.Conv2d(1, 20, 5)
+        self.conv2 = nn.Conv2d(20, 50, 5)
         self.fc1 = nn.Linear(4*4*50, 500)
         self.fc2 = nn.Linear(500, 10)
 
